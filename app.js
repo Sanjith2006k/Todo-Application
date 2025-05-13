8787ab8b4f7982a4c558f1e7a60eeb96cf641a64
Sanjith <ssanjithkumar2006@gmail.com>
2025-05-13 10:39:36 +0000

    Add remove todo feature

--- app.js
+++ app.js
@@ -8,12 +8,27 @@ function addTodo() {
     renderTodos();
   }
 }
+
+function removeTodo(index) {
+  todos.splice(index, 1);
+  renderTodos();
+}
+
 function renderTodos() {
   const list = document.getElementById("todo-list");
   list.innerHTML = "";
-  todos.forEach((todo) => {
+  todos.forEach((todo, index) => {
     const listitem = document.createElement("li");
     listitem.textContent = todo;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Delete";
+    removeBtn.style.marginLeft = "10px";
+    removeBtn.style.color = "red";
+    removeBtn.onclick = function () {
+      removeTodo(index);
+    };
+    listitem.appendChild(removeBtn);
     list.appendChild(listitem);
   });
 }
